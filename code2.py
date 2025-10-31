d416d56bf69ee76da63fc06aeac5bf4d04ff8694
Devanshi-07-code <devanshitaekwondo@gmail.com>
2025-10-31 11:10:01 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create a vector (1D array)
+v = np.array([1, 2, 3])
+print("Vector v:\n", v)
+
+# Create a column vector (2D array)
+v_col = np.array([[1], [2], [3]])
+print("\nColumn vector v_col:\n", v_col)
+
+# Create a matrix (2D array)
+A = np.array([[1, 2, 3],
+              [4, 5, 6]])
+print("\nMatrix A:\n", A)
+
+
